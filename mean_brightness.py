b3a656024455b4cf154adc8fae3283ebac8eb065
salt-die <naclmolecule@gmail.com>
2019-08-16 10:34:38 -0500

    updated to f-strings

--- mean_brightness.py
+++ mean_brightness.py
@@ -13,13 +13,13 @@ unicodestring = "".join([chr(s) for s in range(32,127)])+"".join([chr(s) for s i
 for i,character in enumerate(unicodestring):
     subprocess.run(["convert", "-background","black", "-fill","white", \
                     "-font", font, "-pointsize","72",\
-                    "label:"+character, "{}.png".format(i)])
+                    "label:"+character, f"{i}.png"])
 
 #Measure the mean brightness of each character in the png and save as a dict
 #Brightness is measured with imagemagick
 meanbrightness = {}
 for i, character in enumerate(unicodestring):
-    cmd = ["identify", "-format", "\"%[fx:mean]\"", "{}.png".format(i)]
+    cmd = ["identify", "-format", "\"%[fx:mean]\"", f"{i}.png"]
     meanbrightness[character]=subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0].decode("utf-8")
 
 brightjson = json.dumps(meanbrightness)
