6c0fc46dbfdd51d34b594231fdc1b7148dd872c1
salt-die <naclmolecule@gmail.com>
2019-09-10 17:51:16 -0500

    class attributes removed

--- CyclicTuple.py
+++ CyclicTuple.py
@@ -3,13 +3,11 @@ class CyclicTuple:
     """
     CyclicTuples are tuples that "wrap-around", e.g., (1,2,3,4) == (3,4,1,2) is
     true, but (1,2,3,4) == (1,2,4,3) is not.
-    
+
     To facilitate fast comparison of CyclicTuples, CyclicTuples store their
     canonical form (specifically, the least shift) -- fast comparison of
     shifts is what necessitated this class.
     """
-    __unshifted = None
-    __canonical = None
     def __init__(self, *items):
         self.__unshifted = tuple(items)
         self.__canonical = min(items[i:] + items[:i]\
@@ -23,10 +21,10 @@ class CyclicTuple:
             return self.__canonical == other._CyclicTuple__canonical
         except AttributeError:
             return False
-    
+
     def index(self, item):
         return self.__unshifted.index(item)
-    
+
     #Return a list of the indices of all occurences of an item in self
     def indices(self, item):
         return [i for i, n in enumerate(self.__unshifted) if n==item]
@@ -34,7 +32,6 @@ class CyclicTuple:
     #Return a list of unique items in self -- items in no particular order
     def items(self):
         return list({*self.__unshifted})
-    
+
     def __getitem__(self, key):
         return self.__unshifted[key]
-    
