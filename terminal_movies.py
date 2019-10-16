d77148e2432d2cee4897571826e999aaad7029e1
salt-die <naclmolecule@gmail.com>
2019-10-16 14:10:46 -0500

    audio added

--- terminal_movies.py
+++ terminal_movies.py
@@ -4,10 +4,11 @@ Watch movies in your terminal with this handy video-to-ascii converter.
 Play movie like so:
 >>>python3 terminal_movies.py path/to/movie
 """
-
-import cv2
-import curses
 import argparse
+import curses
+import cv2
+from ffpyplayer.player import MediaPlayer
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("path", help="path to movie")
@@ -19,8 +20,12 @@ scale = 255/len(ascii_map) + .05  #Add a small amount to prevent key errors
 
 def main(screen):
     init_curses(screen)
+
     movie = cv2.VideoCapture(path)
+    audio = MediaPlayer(path)
+    audio.get_frame() #Start audio
     running = read_flag = True
+
     while read_flag and running:
         screen_height, screen_width = screen.getmaxyx()
         read_flag, frame = movie.read()
@@ -32,8 +37,14 @@ def main(screen):
             screen.addstr(row_num, 0,
                           ''.join(ascii_map[int(color/scale)] for color in row[:-1]))
         screen.refresh()
+
+        audio_time = audio.get_pts() * 1000
+        while audio_time - movie.get(cv2.CAP_PROP_POS_MSEC) > 1:
+            movie.read()
+
         running = screen.getch() != ord('q')
 
+    audio.close_player()
     movie.release()
 
 def init_curses(screen):
