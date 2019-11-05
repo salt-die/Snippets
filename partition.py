6b643c87c388d7af7bc4e122f8500f205e50b746
salt-die <naclmolecule@gmail.com>
2019-11-04 21:24:52 -0600

    removed 2nd version

--- partition.py
+++ partition.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Generate all partitions of an iterable.
-Version one returns a generator, version 2 returns a list.
 Taken from somewhere on the Stack.
 """
 def partition(collection):
@@ -17,17 +16,3 @@ def partition(collection):
             yield smaller[:n] + [[first] + subset] + smaller[n+1:]
         # put `first` in its own subset
         yield [[first]] + smaller
-
-def partition2(collection):
-    if len(collection) == 1:
-        return [[collection]]
-
-    first = collection[0]
-    subparts = []
-    for smaller in partition2(collection[1:]):
-        # insert `first` in each of the subpartition's subsets
-        for n, subset in enumerate(smaller):
-            subparts.append(smaller[:n] + [[first] + subset] + smaller[n+1:])
-        # put `first` in its own subset
-        subparts.append([[first]] + smaller)
-    return subparts
