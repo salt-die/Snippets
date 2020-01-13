f7c4936dd0038facfeebce60de8b55b422c4ede6
salt-die <naclmolecule@gmail.com>
2020-01-13 17:36:48 -0600

    compactified a bit

--- animated_gif.py
+++ animated_gif.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Often I generate a large number of numbered pngs, e.g., 000.png, 001.png...,
 that I want to stitch together into an animated gif.  This code does that.
@@ -8,7 +7,5 @@ import imageio
 
 png_dir = 'your png directory'
 images = [imageio.imread(os.path.join(png_dir, file_name))
-          for file_name in sorted(os.listdir(png_dir))
-          if file_name.endswith('.png')]
+          for file_name in sorted(os.listdir(png_dir)) if file_name.endswith('.png')]
 imageio.mimsave(png_dir + 'title.gif', images, duration = .1)
-
