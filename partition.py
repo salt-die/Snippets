c1b602015fee7ea7a70d4e2aa4b8aed2ca90de4a
salt-die <naclmolecule@gmail.com>
2020-03-07 02:02:00 -0600

    more partition recipes

--- partition.py
+++ partition.py
@@ -13,6 +13,10 @@ def partition(collection):
         # put `first` in its own subset
         yield [[first]] + smaller
 
+### Following recipes from
+### https://stackoverflow.com/questions/10035752/elegant-python-code-for-integer-partitioning
+### Credit to David Vanderschel
+
 def partitions(n, m=None):
     """Generate all integer partitions of n with part size no bigger than m."""
     if m is None or m >= n:
@@ -24,3 +28,26 @@ def partitions(n, m=None):
     for m0 in range(start, 0, -1):
         for subpartition in partitions(n - m0, m0):
             yield [size] + subpartition
+
+def sized_partitions(n, k, m=None):
+    """Partition n into k parts with a max part of m."""
+    if k == 1:
+        yield [n]
+        return
+
+    start = n - k + 1
+    if not (m is None or m > start):
+        start = m
+
+    for m0 in range(start, (n - 1) // k, -1):
+        for subpartition in sized_partitions(n - m0, k - 1, m0):
+            yield [m0] + subpartition
+
+def partitions_upto(n, max_val=100000, max_len=100000):
+    """ generator of partitions of sum with limits on values and length """
+    if n <= max_val:
+        yield [n]
+
+    for m0 in range(min(n - 1, max_val), max(0, (n - 1) // max_len), -1):
+        for subpartition in partitions_upto(n - m0, m0, max_len - 1):
+            yield [m0] + subpartition
