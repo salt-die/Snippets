71446fbc13712e5fa31223c22060732985e279ff
salt-die <naclmolecule@gmail.com>
2020-04-09 14:29:17 -0500

    alternative implementation

--- dynamic_dict.py
+++ dynamic_dict.py
@@ -9,3 +9,13 @@ class DynamicDict(dict):
         if key not in self:
             super().__setitem__(key, self.func(key))
         return super().__getitem__(key)
+
+
+# Alternatively:
+from collections import defaultdict
+
+
+class dynamicdict(defaultdict):
+    def __missing__(self, key):
+        self[key] = self.default_factory(key)
+        return self[key]
