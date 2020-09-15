1bb2fd5997b4424e8900e1d3083d02d5aa4ce323
salt-die <53280662+salt-die@users.noreply.github.com>
2020-09-15 01:46:26 -0500

    added sentinal, simplified example

--- maybe.py
+++ maybe.py
@@ -1,9 +1,11 @@
+Null = object()  # Sentinel, None may be a valid attribute value
+
 class MaybeMeta(type):
     """This metaclass ensures Maybe(None) is singleton."""
     nothing = None
 
     def __call__(cls, value):
-        if value is None:
+        if value is Null:
             if MaybeMeta.nothing is None:
                 MaybeMeta.nothing = super(MaybeMeta, cls).__call__(value)
             return MaybeMeta.nothing
@@ -16,33 +18,19 @@ class Maybe(metaclass=MaybeMeta):
         self.__value = value
 
     def __getattr__(self, attr):
-        if self.__value is None:
+        if self.__value is Null:
             return self
 
-        return Maybe(getattr(self.__value, attr, None))
+        return Maybe(getattr(self.__value, attr, Null))
 
     def __or__(self, other):
-        if self.__value is None:
+        if self.__value is Null:
             return other
         return self.__value
 
 
 if __name__ == "__main__":
-    class User:
-        def __init__(self, name, friends):
-            self.name = name
-            self.friends = friends
-
-    class Friends:
-        def __init__(self, *friends):
-            for friend in friends:
-                setattr(self, friend, friend)
-
-    class Users:
-        def __init__(self, *users):
-            for user in users:
-                setattr(self, user.name, user)
-
-    users = Users(User('bob', Friends('sue')))
+    class U: ...
+    users = U(); users.bob = U(); users.bob.friends = U(); users.bob.friends.sue = 'sue'
     print(Maybe(users).bob.friends.sue | 'mary')  # attributes all exist, prints 'sue'
     print(Maybe(users).bob.friends.mary | 'peggy')  # mary doesn't exist, prints 'peggy'
