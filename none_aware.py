d1d2d0abdb71f50b5da3ae2021cad4aaf833bc7c
salt-die <53280662+salt-die@users.noreply.github.com>
2020-09-27 21:58:44 -0500

    left-over code removed

--- none_aware.py
+++ none_aware.py
@@ -5,11 +5,8 @@ def maybe(val):
     When assigning to some variable, say `x`, maybe(val) will return val if val is not None else will return x.
 
     Warning:: Variable must exist prior to this assignment.  Will raise error if not used in an assignment or if
-    some name or attribute on the lhs of the assignment isn't defined.
+    some name or attribute on the lhs of the assignment isn't defined.  Not suitable for multi-line expressions.
     """
-    if other is not None:
-        return val if val is not None else other
-
     parent_frame = currentframe().f_back
     line = getframeinfo(parent_frame).code_context[0]
     var, sep, _ = line.partition('=')
