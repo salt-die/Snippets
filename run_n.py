c1edd228c9a5fb4f140083295113318496da7887
salt-die <53280662+salt-die@users.noreply.github.com>
2020-11-03 01:05:02 -0600

    previous solution did not work

--- run_n.py
+++ run_n.py
@@ -1,30 +1,30 @@
-from collections import Counter
-from functools import singledispatch, wraps
-from types import MethodType, FunctionType
+"""I'm unsure of the best way to let a decorator determine if the function it decorates is
+defined inside a class or not.  This is one hack-y solution:  see if the first parameter of the function is `self`.
+"""
 
+from collections import Counter
+from functools import wraps
+from inspect import signature
 
 def run_n(n):
     """Decorator that allows a function or method to only run n times."""
-    @singledispatch
-    def deco(f: FunctionType):
-        @wraps(f)
-        def wrapper(*args, **kwargs):
-            if wrapper.nruns < n:
-                wrapper.nruns += 1
-                return f(*args, **kwargs)
-        wrapper.nruns = 0
-        return wrapper
-
-    @deco.register
-    def _(f: MethodType):
+    def deco(f):
         instance_run_counts = Counter()
 
         @wraps(f)
-        def wrapper(self, *args, **kwargs):
-            if instance_run_counts[(i := id(self))] < n:
-                instance_run_counts[i] += 1
-                return f(self, *args, **kwargs)
+        def wrapper(*args, **kwargs):
+            # determine if a function was defined in a class
+            if (params := signature(f).parameters) and next(iter(params)) == 'self':
+                if instance_run_counts[id(args[0])] < n:
+                    instance_run_counts[id(args[0])] += 1
+                    return f(*args, **kwargs)
+                return
 
+            if wrapper.nruns < n:
+                wrapper.nruns += 1
+                return f(*args, **kwargs)
+
+        wrapper.nruns = 0
         return wrapper
 
     return deco
