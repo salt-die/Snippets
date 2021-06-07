112a1d1bec2571dd7ba251f49a928508ab802ebf
salt-die <53280662+salt-die@users.noreply.github.com>
2021-06-06 22:12:30 -0500

    Rewrote without singleton meta.

--- maybe.py
+++ maybe.py
@@ -1,53 +1,41 @@
-Null = object()
-
-
-class MaybeMeta(type):
-    """Cache Maybe(Null)"""
-    Nothing = None
-
-    def __call__(cls, val):
-        if val is Null:
-            if MaybeMeta.Nothing is None:
-                MaybeMeta.Nothing = super(MaybeMeta, cls).__call__(val)
-            return MaybeMeta.Nothing
-        return super(MaybeMeta, cls).__call__(val)
-
-
-class Maybe(metaclass=MaybeMeta):
-    """A class for Chaining attributes that may or may not exist.
+class Maybe:
+    """
+    A class for Chaining attributes that may or may not exist.
     Say we want to assign `x` the value from `users.bob.friends[1].name` if it exists or None if it doesn't:
     `x = Maybe(users).bob.friends[1].name | None`
     """
     def __init__(self, val):
-        self.__value = val
+        object.__setattr__(self, '_Maybe__value', val)
 
     def __getattr__(self, attr):
-        return Maybe(getattr(self.__value, attr, Null))
+        try:
+            return Maybe(getattr(self.__value, attr))
+        except AttributeError:
+            return NOPE
 
     def __setattr__(self, attr, val):
-        if attr == '_Maybe__value':
-            return super().__setattr__(attr, val)
-
-        if self.__value is not Null:
+        if self is not NOPE:
             setattr(self.__value, attr, val)
 
     def __call__(self, *args, **kwargs):
         try:
             return Maybe(self.__value(*args, **kwargs))
         except TypeError:
-            return Maybe(Null)
+            return NOPE
 
     def __getitem__(self, key):
         try:
             return Maybe(self.__value[key])
         except (TypeError, IndexError, KeyError):
-            return Maybe(Null)
+            return NOPE
 
     def __or__(self, other):
-        if self.__value is Null:
+        if self is NOPE:
             return other
         return self.__value
 
+
+NOPE = Maybe(object())
 
 if __name__ == '__main__':
     class U: ...
