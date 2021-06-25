e0e76bebf7dde7b8b835e7d0347efb305f369870
salt-die <53280662+salt-die@users.noreply.github.com>
2021-06-25 16:04:02 -0500

    Updated to use `contextmanager` decorator.

--- recursionlimit.py
+++ recursionlimit.py
@@ -1,23 +1,14 @@
 """
-Context manager for increasing the recursion limit from:
-
-https://stackoverflow.com/questions/3323001/what-is-the-maximum-recursion-depth-in-python-and-how-to-increase-it
-
-Using it looks like:
-
-with RecursionLimit(2000):
-    something()
+Context manager for increasing the recursion limit.
 """
-
+from contextlib import contextmanager
 import sys
 
-class RecursionLimit:
-    def __init__(self, limit):
-        self.limit = limit
-        self.old_limit = sys.getrecursionlimit()
+@contextmanager
+def recursion_limit(limit):
+    old_limit = sys.getrecursionlimit()
 
-    def __enter__(self):
-        sys.setrecursionlimit(self.limit)
-
-    def __exit__(self, type, value, tb):
-        sys.setrecursionlimit(self.old_limit)
+    try:
+        sys.setrecursionlimit(limit)
+    finally:
+        sys.setrecursionlimit(old_limit)
